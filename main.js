bde47c2ccbb21ec7cb565cb4854fffa985b690fe
krolique <truekrolique@gmail.com>
2021-04-12 18:49:25 +0300

    homework-4 v1.1

--- main.js
+++ main.js
@@ -6,35 +6,42 @@
 const arenas = document.querySelector('.arenas');
 const rndmButton = document.querySelector('.button');
 
-const kungLao = new Fighter('player1', 'Kung Lao', 90, 'https://static.wikia.nocookie.net/mkwikia/images/1/1b/Kunglao_mk3.gif', ['hat']);
-const jax = new Fighter('player2', 'Jax', 85, 'https://static.wikia.nocookie.net/mkwikia/images/4/4e/JAX.gif', ['left hand', 'right hand']);
-const scorpion = new Fighter('player1', 'Scorpion', 50, 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', ['kunai chain', 'hellfire']);
-const kitana = new Fighter('player1', 'Kitana', 90, 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif', ['steel fan', 'one more steel fan']);
-const liuKang = new Fighter('player2', 'Liu Kang', 70, 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif', ['left leg', 'right leg']);
-const sonya = new Fighter('player1', 'Sonya', 80, 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', ['gun', 'some weapon']);
-const subZero = new Fighter('player2', 'Sub-Zero', 80, 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif', ['ice blast', 'ice clone', 'many other ice stuff']);
+const kungLao = new Fighter('player1', 'Kung Lao', 100, 'https://static.wikia.nocookie.net/mkwikia/images/1/1b/Kunglao_mk3.gif', ['hat']);
+const jax = new Fighter('player2', 'Jax', 100, 'https://static.wikia.nocookie.net/mkwikia/images/4/4e/JAX.gif', ['left hand', 'right hand']);
+const scorpion = new Fighter('player1', 'Scorpion', 100, 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', ['kunai chain', 'hellfire']);
+const kitana = new Fighter('player1', 'Kitana', 100, 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif', ['steel fan', 'one more steel fan']);
+const liuKang = new Fighter('player2', 'Liu Kang', 100, 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif', ['left leg', 'right leg']);
+const sonya = new Fighter('player1', 'Sonya', 100, 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', ['gun', 'some weapon']);
+const subZero = new Fighter('player2', 'Sub-Zero', 100, 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif', ['ice blast', 'ice clone', 'many other ice stuff']);
 
 const player1 = new CreatePlayer(kungLao);
 const player2 = new CreatePlayer(jax);
+
+player1.fighter.elhp = elHP;
+player2.fighter.elhp = elHP;
+player1.fighter.renderhp = renderHP;
+player2.fighter.renderhp = renderHP;
+player1.fighter.changehp = changeHP;
+player2.fighter.changehp = changeHP;
 
 arenas.appendChild(player1.buildDOM());
 arenas.appendChild(player2.buildDOM());
 
 const randomHP = () => Math.floor(Math.random() * 20) + 1;
 
-function changeHp(value) {
+function changeHP(value) {
     value = randomHP();
 
     const enemy = player2.fighter;
     enemy.hp -= value;
 
     if (enemy.hp > 0) {
-        renderHP.call(enemy);
+        enemy.renderhp();
         console.log(`${enemy.name} HP left:`, enemy.hp);
     }
     else {
         enemy.hp = 0;
-        renderHP.call(enemy);
+        enemy.renderhp();
         document.querySelector('.winnerBar').classList.toggle('show');
         document.querySelector('.winnerBar').innerHTML = `${player1.fighter.name} wins`;
         rndmButton.disabled = true;
@@ -49,4 +56,7 @@ function elHP() {
     return document.querySelector(`.${this.player} .life`);
 }
 
-rndmButton.onclick = changeHp;
+rndmButton.onclick = function () {
+    player2.fighter.changehp();
+};
+
