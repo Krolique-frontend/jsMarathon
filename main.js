c9061a14cb26800a624c415173ba506744af24ae
krolique <truekrolique@gmail.com>
2021-04-12 13:22:09 +0300

    restore

--- main.js
+++ main.js
@@ -3,17 +3,53 @@
 // alert('fightM...');
 // console.log('fight...');
 
-let kungLao = new Fighter('Kung Lao', 100, 'https://static.wikia.nocookie.net/mkwikia/images/1/1b/Kunglao_mk3.gif', ['hat']);
-let jax = new Fighter('Jax', 100, 'https://static.wikia.nocookie.net/mkwikia/images/4/4e/JAX.gif', ['left hand', 'right hand']);
-let scorpion = new Fighter('Scorpion', 50, 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', ['kunai chain', 'hellfire']);
-let kitana = new Fighter('Kitana', 90, 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif', ['steel fan', 'one more steel fan']);
-let liuKang = new Fighter('Liu Kang', 70, 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif', ['left leg', 'right leg']);
-let sonya = new Fighter('Sonya', 80, 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', ['gun', 'some weapon']);
-let subZero = new Fighter('Sub-Zero', 80, 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif', ['ice blast', 'ice clone', 'many other ice stuff']);
+const arenas = document.querySelector('.arenas');
+const rndmButton = document.querySelector('.button');
 
+const kungLao = new Fighter('player1', 'Kung Lao', 90, 'https://static.wikia.nocookie.net/mkwikia/images/1/1b/Kunglao_mk3.gif', ['hat']);
+const jax = new Fighter('player2', 'Jax', 85, 'https://static.wikia.nocookie.net/mkwikia/images/4/4e/JAX.gif', ['left hand', 'right hand']);
+const scorpion = new Fighter('player1', 'Scorpion', 50, 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', ['kunai chain', 'hellfire']);
+const kitana = new Fighter('player1', 'Kitana', 90, 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif', ['steel fan', 'one more steel fan']);
+const liuKang = new Fighter('player2', 'Liu Kang', 70, 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif', ['left leg', 'right leg']);
+const sonya = new Fighter('player1', 'Sonya', 80, 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', ['gun', 'some weapon']);
+const subZero = new Fighter('player2', 'Sub-Zero', 80, 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif', ['ice blast', 'ice clone', 'many other ice stuff']);
 
-let player1 = new CreatePlayer('player1', scorpion).buildDOM();
-let player2 = new CreatePlayer('player2', subZero).buildDOM();
+const player1 = new CreatePlayer(kungLao);
+const player2 = new CreatePlayer(jax);
 
-scorpion.attack();
-subZero.attack();
+arenas.appendChild(player1.buildDOM());
+arenas.appendChild(player2.buildDOM());
+
+function changeHp() {
+    let offender, defender;
+
+    if (Math.random() > 0.5) {
+        offender = player1.fighter;
+        defender = player2.fighter;
+    } else {
+        offender= player2.fighter;
+        defender = player1.fighter;
+    }
+
+    const life = document.querySelector(`.${defender.player} .life`);
+    const randomHP = Math.floor( 20 - Math.random() * 20 );
+
+    if (randomHP === 0) console.log(`${defender.name} block`);
+    else  defender.hp -= randomHP;
+
+    if (defender.hp > 0) {
+        life.style.width = `${defender.hp}%`;
+        console.log(`${defender.name} HP left:`, defender.hp);
+    }
+    else {
+        life.style.width = `0%`;
+        document.querySelector('.winnerBar').classList.toggle('show');
+        document.querySelector('.winnerBar').innerHTML = `${offender.name} wins`;
+        rndmButton.disabled = true;
+    }
+}
+
+rndmButton.onclick = changeHp;
+
+// scorpion.attack();
+// subZero.attack();
