1429867aec1ba74aa60b1625c451d804c6ad4240
krolique <truekrolique@gmail.com>
2021-04-15 20:06:39 +0300

    homework-5

--- main.js
+++ main.js
@@ -1,56 +1,25 @@
 "use strict";
-
-// alert('fightM...');
-// console.log('fight...');
-
 const arenas = document.querySelector('.arenas');
-const rndmButton = document.querySelector('.button');
+const formFight = document.querySelector('.control');
 
-const kungLao = new Fighter('player1', 'Kung Lao', 100, 'https://static.wikia.nocookie.net/mkwikia/images/1/1b/Kunglao_mk3.gif', ['hat']);
-const jax = new Fighter('player2', 'Jax', 100, 'https://static.wikia.nocookie.net/mkwikia/images/4/4e/JAX.gif', ['left hand', 'right hand']);
-const scorpion = new Fighter('player1', 'Scorpion', 100, 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif', ['kunai chain', 'hellfire']);
-const kitana = new Fighter('player1', 'Kitana', 100, 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif', ['steel fan', 'one more steel fan']);
-const liuKang = new Fighter('player2', 'Liu Kang', 100, 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif', ['left leg', 'right leg']);
-const sonya = new Fighter('player1', 'Sonya', 100, 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif', ['gun', 'some weapon']);
-const subZero = new Fighter('player2', 'Sub-Zero', 100, 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif', ['ice blast', 'ice clone', 'many other ice stuff']);
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20
+};
+const ATTACK = ['head', 'body', 'foot'];
 
-const player1 = new CreatePlayer(kungLao);
-const player2 = new CreatePlayer(jax);
+const p1 = characters.find(char => char.name === 'Sonya');
+const p2 = characters.find(char => char.name === 'Jax');
 
-player1.fighter.elhp = elHP;
-player2.fighter.elhp = elHP;
-player1.fighter.renderhp = renderHP;
-player2.fighter.renderhp = renderHP;
-player1.fighter.changehp = changeHP;
-player2.fighter.changehp = changeHP;
+const player1 = new Fighter(p1, 'player1');
+const player2 = new Fighter(p2, 'player2');
 
-arenas.appendChild(player1.buildDOM());
-arenas.appendChild(player2.buildDOM());
+arenas.appendChild(createPlayer.call(player1));
+arenas.appendChild(createPlayer.call(player2));
 
+formFight.onsubmit = event => {
+    event.preventDefault();
 
-
-// Пока оставил это всё для интриги, уберу потом =)
-rndmButton.onclick = function () {
-    const punch = Math.random() > 0.5 ? 1 : 0;
-
-    if (punch === 1) {
-        player1.fighter.changehp();
-        chatLog.call(player1.fighter);
-    }
-    else {
-        player2.fighter.changehp();
-        chatLog.call(player2.fighter);
-    }
-
-    if (player2.fighter.hp === 0 || player1.fighter.hp === 0) {
-        const winner = player1.fighter.hp === 0 ? player2.fighter : player1.fighter;
-
-        document.querySelector('.winnerBar').classList.toggle('show');
-        document.querySelector('.winnerBar').innerHTML = `${winner.name} wins`;
-
-        rndmButton.disabled = true;
-
-        document.querySelector('.control').appendChild(createReloadButton());
-        document.querySelectorAll('.button')[1].onclick = restart;
-    }
+    fight();
 };
