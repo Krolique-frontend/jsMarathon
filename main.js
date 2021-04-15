7877ba25f0bc8962d9d11ce57b8ae37aa6c9151e
krolique <truekrolique@gmail.com>
2021-04-15 21:00:05 +0300

    homework-5 v1.1

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 "use strict";
+const sel1 = document.getElementById('select1');
+const sel2 = document.getElementById('select2');
+const choose = document.querySelector('.choose');
+
+characters.forEach(char => {
+    const opt = document.createElement('option');
+    opt.innerText = char.name;
+    sel1.appendChild(opt);
+});
+
+characters.forEach(char => {
+    const opt = document.createElement('option');
+    opt.innerText = char.name;
+    sel2.appendChild(opt);
+});
+
 const arenas = document.querySelector('.arenas');
 const formFight = document.querySelector('.control');
 
@@ -9,14 +25,34 @@ const HIT = {
 };
 const ATTACK = ['head', 'body', 'foot'];
 
-const p1 = characters.find(char => char.name === 'Sonya');
-const p2 = characters.find(char => char.name === 'Jax');
+let p1, p2, player1, player2;
 
-const player1 = new Fighter(p1, 'player1');
-const player2 = new Fighter(p2, 'player2');
+sel1.onchange = () => {
+    p1 = characters.find(char => char.name === sel1.value);
+    player1 = new Fighter(p1, 'player1');
+    arenas.appendChild(createPlayer.call(player1));
+};
 
-arenas.appendChild(createPlayer.call(player1));
-arenas.appendChild(createPlayer.call(player2));
+sel2.onchange = () => {
+    p2 = characters.find(char => char.name === sel2.value);
+    player2 = new Fighter(p2, 'player2');
+    arenas.appendChild(createPlayer.call(player2));
+};
+
+applyChoice.onclick = () => {
+    if (player1 && player2) choose.style.display = 'none';
+    else alert('Choose your fighter!');
+};
+
+// HardCode
+// const p1 = characters.find(char => char.name === 'Sonya');
+// const p2 = characters.find(char => char.name === 'Jax');
+
+// const player1 = new Fighter(p1, 'player1');
+// const player2 = new Fighter(p2, 'player2');
+
+// arenas.appendChild(createPlayer.call(player1));
+// arenas.appendChild(createPlayer.call(player2));
 
 formFight.onsubmit = event => {
     event.preventDefault();
